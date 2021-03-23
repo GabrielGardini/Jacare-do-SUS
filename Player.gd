edfc49414b2c029b912601eda9a286568b52c4f8
Paulo <p242863@dac.unicamp.br>
2021-03-22 21:58:46 -0300

    Adiciona movimentacao ao player

---
+++ Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+var motion = Vector2()
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		motion.x = 100
+		move_and_slide(motion)
+	elif Input.is_action_pressed("ui_left"):
+		motion.x = -100
+		move_and_slide(motion)
+	elif Input.is_action_pressed("ui_up"):
+		motion.y = -100
+		move_and_slide(motion)
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = 100
+		move_and_slide(motion)
+	else:
+		motion.x = 0
+		motion.y = 0
