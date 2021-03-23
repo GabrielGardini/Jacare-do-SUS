ae3a85b814dee232f142f086960128deea1144ab
Paulo <p242863@dac.unicamp.br>
2021-03-23 14:27:58 -0300

    Adiciona constante 'speed'

--- Player.gd
+++ Player.gd
@@ -1,20 +1,21 @@
 extends KinematicBody2D
 
 var motion = Vector2()
+const speed = 100
 
 func _physics_process(delta):
 	look_at(get_global_mouse_position())
 	if Input.is_action_pressed("ui_right"):
-		motion.x = 100
+		motion.x = speed
 		move_and_slide(motion)
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = -100
+		motion.x = -speed
 		move_and_slide(motion)
 	elif Input.is_action_pressed("ui_up"):
-		motion.y = -100
+		motion.y = -speed
 		move_and_slide(motion)
 	elif Input.is_action_pressed("ui_down"):
-		motion.y = 100
+		motion.y = speed
 		move_and_slide(motion)
 	else:
 		motion.x = 0
