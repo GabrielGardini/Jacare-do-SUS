ed99ba72fc3a972bbd11dd1bd219e8024660d740
Paulo <p242863@dac.unicamp.br>
2021-03-25 18:38:53 -0300

    Reorganiza as pastas; adicionada pasta 'scripts'

--- level design/level 1/bullet.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-var speed = 500
-
-func _physics_process(delta):
-	position += transform.x * speed * delta
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,16 +1,22 @@
 extends KinematicBody2D
+var move = Vector2.ZERO
+var player = null
+var speed = 1
 
+func _physics_process(delta):
+	move = Vector2.ZERO
+	if player != null:
+		move = position.direction_to(player.position) * speed
+	else:
+		move = Vector2.ZERO
+	
+	move = move.normalized()
+	move = move_and_collide(move)
+	
+func _on_Area2D_body_entered(body):
+	if body != self:
+		player = body
+		
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Area2D_body_exited(body):
+	player = null
