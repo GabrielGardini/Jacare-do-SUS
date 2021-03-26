fb1a934d53809643f0d0fef7f26f8ae3a5df2762
Paulo <p242863@dac.unicamp.br>
2021-03-26 01:14:08 -0300

    Cria o inimigo 'virus' (script deve ser revisto)

--- scripts/bullet.gd
+++ scripts/bullet.gd
@@ -1,13 +1,18 @@
 extends Area2D
+var look = null
 
 var speed = 500
-
 func _physics_process(delta):
 	position += transform.x * speed * delta
-
-
-
+#	look_at(get_global_mouse_position())
+#	look_at(get_global_position())
+	pass
 func _on_vacina_body_entered(body):
 	if body.is_in_group("trash_can"):
 		queue_free()
 	pass # Replace with function body.
+
+#func vac_direction():
+#	look=look_at(get_global_mouse_position())
+#	look=null
+#	pass
--- scripts/virus.gd
+++ scripts/virus.gd
@@ -1,3 +1,6 @@
 extends KinematicBody2D
 
-
+func _on_hitbox_body_entered(body):
+	if body.is_in_group("bullet"):
+		queue_free()
+	pass
