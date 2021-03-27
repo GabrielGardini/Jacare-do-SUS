2612a22311c37cb83cfb8cca006c822ec9e60a19
Paulo <p242863@dac.unicamp.br>
2021-03-27 12:03:22 -0300

    Adiciona movimentação ao virus (INCOMPLETO)

--- scripts/virus.gd
+++ scripts/virus.gd
@@ -1,4 +1,9 @@
 extends KinematicBody2D
+var speed = -75
+
+func _process(delta):
+	translate(Vector2(speed,0) * delta)
+	pass
 
 
 func _on_virus_hitbox_area_entered(area):#Tira o virus da cena caso ele seja atingido pela vacina
