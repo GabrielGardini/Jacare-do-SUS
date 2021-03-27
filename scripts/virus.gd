04d77943ab599406e229e12d0a38cbb4f67d8c58
Paulo <p242863@dac.unicamp.br>
2021-03-27 10:56:13 -0300

    Adiciona comentários aos scripts

--- scripts/virus.gd
+++ scripts/virus.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 
-func _on_virus_hitbox_area_entered(area):
+func _on_virus_hitbox_area_entered(area):#Tira o virus da cena caso ele seja atingido pela vacina
 	if area.is_in_group("bullet"):
 		queue_free()
-	pass # Replace with function body.
+	pass
