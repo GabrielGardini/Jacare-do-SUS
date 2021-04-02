a569b28b0b20307ed7a7e950df0d87665acd649a
Paulo <p242863@dac.unicamp.br>
2021-04-01 22:25:28 -0300

    Renomeia os labels

--- scripts/cena1.gd
+++ scripts/cena1.gd
@@ -18,14 +18,14 @@ func _on_enemy_hitbox_area_entered(area):#Pontua negativamente caso a vacina ent
 		points -= 1000
 		if points <= 0:
 			points = 0
-		$CanvasLayer2/score_label.set_text("score: "+str(points))
+		$score/score_label.set_text("score: "+str(points))
 	pass
 
 func _on_virus_hitbox_area_entered(area):#Pontua positivamente caso a vacina entre em contato com o vírus
 	if area.is_in_group("bullet"):
 		$virus_kill.play()
 		points += 100
-		$CanvasLayer2/score_label.set_text("score: "+str(points))
+		$score/score_label.set_text("score: "+str(points))
 
 func _on_spawn_timer_timeout():
 	virus = PRE_VIRUS.instance()	
@@ -41,6 +41,6 @@ func _on_limit_area_entered(area):
 			if points <= 0:
 				points = 0
 			$shot_trash.play()
-			$CanvasLayer2/score_label.set_text("score: "+str(points))
+			$score/score_label.set_text("score: "+str(points))
 		pass # Replace with function body.
 	
