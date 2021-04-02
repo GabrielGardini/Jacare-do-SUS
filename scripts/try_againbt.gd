62757025e756aef7368ed60d253d84591b7d9e13
Paulo <p242863@dac.unicamp.br>
2021-04-02 00:14:11 -0300

    Corrige bug do .exe

--- scripts/try_againbt.gd
+++ scripts/try_againbt.gd
@@ -10,7 +10,9 @@ func _process(delta):
 		get_tree().paused = true
 		 
 	if Input.is_action_pressed("ui_cancel"):
+		get_tree().paused = false
 		get_tree().change_scene("res://menu.tscn")
+		
 	
 func _on_player_hitbox_area_entered(area):
 	if area.is_in_group("trash_can"):
